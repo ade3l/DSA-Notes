0cf2fcead34caf604315ef44cca52f4ac55ca2bc
Adeel <71775487+ade3l@users.noreply.github.com>
2021-04-28 22:03:01 +0300

    i dont remember

---
+++ 7. Doubly Linked List/2. Insertion/cmake-build-debug/CMakeFiles/2__Insertion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2__Insertion.exe"
+  "2__Insertion.exe.manifest"
+  "2__Insertion.pdb"
+  "CMakeFiles/2__Insertion.dir/main.c.obj"
+  "lib2__Insertion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2__Insertion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
