e10c747d41a8d85eef9f781be5139505e290977d
Adeel <71775487+ade3l@users.noreply.github.com>
2021-04-30 20:28:35 +0300

    insert at beg and end codes added

--- 6. Circular linked list/4. Insertion/main.c
+++ 6. Circular linked list/4. Insertion/main.c
@@ -1,6 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+}*tail;
+void init(){
+    tail=NULL;
+    struct node *new_node;
+    for(int i=1;i<10;i++){
+        new_node=(struct node *)malloc(sizeof(struct node));
+        new_node->next=NULL;
+        new_node->data=10*i;
+        if(tail==NULL){
+            tail=new_node;
+            new_node->next=tail;
+        }
+        else{
+            new_node->next=tail->next;
+            tail->next=new_node;
+            tail=new_node;
+        }
+    }
+}
+void display(){
+    struct node *temp;
+    temp=tail->next;
+    printf("\n");
+    do{
+        printf("%d\t",temp->data);
+        temp=temp->next;
+    }while(temp!=tail->next);
+    printf("\n");
+}
 
+void insertAtBeg(){
+    struct node *new_node;
+    new_node->data=0;//read input from user if you want
+    new_node->next=NULL;
+    if(tail==NULL){
+        //list is empty
+        tail=new_node;
+        new_node->next=tail;
+    }
+    else{
+        new_node->next=tail->next;//new node now points to the first node
+        tail->next=new_node;//last node points now points to the new node as it is now the first node
+    }
+}
+void insertAtEnd(){
+    struct node *new_node;
+    new_node->data=100;//read input from user if you want
+    new_node->next=NULL;
+    if(tail==NULL){
+        tail=new_node;
+        new_node->next=new_node;
+    }
+    else{
+        new_node->next=tail->next;
+        tail->next=new_node;
+        tail=new_node;
+    }
+    //Basically inserting at the end is same as inserting at the beginning but the tail pointer is moved to the new node.
+    //in insert at beginning this isn't done
+}
 int main() {
-    printf("Hello, World!\n");
+    init();
+    printf("the list is:");
+    display();
     return 0;
 }
