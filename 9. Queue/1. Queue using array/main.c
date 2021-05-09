389cdc66fb9a86648525b8bad17249259e817ac1
Adeel <71775487+ade3l@users.noreply.github.com>
2021-05-09 15:07:32 +0300

    Queue using arrays added

--- 9. Queue/1. Queue using array/main.c
+++ 9. Queue/1. Queue using array/main.c
@@ -3,10 +3,75 @@
 int q[N];
 int front=-1;
 int rear=-1;
-void enqueue(int x){
-    
+
+void enqueue(){
+    if(rear==N-1){
+        printf("\nQ is full\n");
+    }
+    else {
+        int x;
+        printf("\nEnter a number");
+        scanf("%d", &x);
+
+        if (front == -1 && rear == -1) {
+            //Currently empty queue
+            front=rear=0;
+        }
+        else{
+            rear++;
+        }
+        q[rear]=x;
+    }
+}
+
+void dequeue(){
+    if(front==-1 && rear==-1){
+        printf("\nQueue is empty");
+    }
+    else{
+        printf("\nThe dequeued element is %d\n",q[front]);
+        if(front==rear){
+            //Only one element is in the q
+            front=rear=-1;
+        }
+        else{
+            front++;
+        }
+    }
+}
+
+void display(){
+    if(front==-1 && rear==-1){
+        printf("Queue is empty");
+    }
+    else{
+        printf("\n");
+        for(int i=front;i<=rear;i++){
+            printf("%d\t",q[i]);
+        }
+        printf("\n");
+    }
+}
+
+void peek(){
+    if(front==-1 && rear==-1){
+        printf("Queue is empty");
+    }
+    else
+        printf("\n%d\n",q[front]);
 }
 int main() {
-    printf("Hello, World!\n");
+    int choice;
+    do{
+        printf("\nOperations:\n1.Enqueue\n2.Dequeue\n3.Display\n4.peek\n0.Quit\n");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:enqueue();break;
+            case 2:dequeue();break;
+            case 3:display();break;
+            case 4:peek();break;
+            default:printf("\nInvalid input\n");break;
+        }
+    }while(choice);
     return 0;
 }
