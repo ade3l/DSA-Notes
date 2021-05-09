2fc1804c370268b65eb132dcd29655702983759e
Adeel <71775487+ade3l@users.noreply.github.com>
2021-05-09 14:54:22 +0300

    Queue file added

---
+++ 9. Queue/1. Queue using array/cmake-build-debug/CMakeFiles/1__Queue_using_array.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1__Queue_using_array.
+# This may be replaced when dependencies are built.
---
+++ 9. Queue/1. Queue using array/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define N 5
+int q[N];
+int front=-1;
+int rear=-1;
+void enqueue(int x){
+    
+}
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
