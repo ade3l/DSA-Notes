a9fe574d8abe4bee973d8feb6f80412f7c749b53
Adeel <71775487+ade3l@users.noreply.github.com>
2021-05-21 15:17:58 +0300

    Added creation and traversal codes

---
+++ 10. Binary Trees/1. Creation and Traversal/cmake-build-debug/CMakeFiles/1__Creation_and_Traversal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1__Creation_and_Traversal.exe"
+  "1__Creation_and_Traversal.exe.manifest"
+  "1__Creation_and_Traversal.pdb"
+  "CMakeFiles/1__Creation_and_Traversal.dir/main.c.obj"
+  "lib1__Creation_and_Traversal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1__Creation_and_Traversal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
