cc6d8c7b5adcc0f5ed79dd4b069d025b967250cb
Adeel <71775487+ade3l@users.noreply.github.com>
2021-06-15 19:33:55 +0300

    BFS initial commit

---
+++ 14. Graphs/2. BFS in adj matrix/cmake-build-debug/CMakeFiles/2__BFS_in_adj_matrix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2__BFS_in_adj_matrix.exe"
+  "2__BFS_in_adj_matrix.exe.manifest"
+  "2__BFS_in_adj_matrix.pdb"
+  "CMakeFiles/2__BFS_in_adj_matrix.dir/main.c.obj"
+  "lib2__BFS_in_adj_matrix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2__BFS_in_adj_matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14. Graphs/2. BFS in adj matrix/cmake-build-debug/CMakeFiles/2__BFS_in_adj_matrix.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 2__BFS_in_adj_matrix.
+# This may be replaced when dependencies are built.
