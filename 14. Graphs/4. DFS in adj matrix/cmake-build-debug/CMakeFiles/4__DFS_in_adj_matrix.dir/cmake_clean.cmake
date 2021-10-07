32547cbdcd4aabe1239135af8509d0347fa7a748
Adeel <71775487+ade3l@users.noreply.github.com>
2021-10-07 10:35:11 +0300

    dfs in adj matrix

---
+++ 14. Graphs/4. DFS in adj matrix/cmake-build-debug/CMakeFiles/4__DFS_in_adj_matrix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4__DFS_in_adj_matrix.exe"
+  "4__DFS_in_adj_matrix.exe.manifest"
+  "4__DFS_in_adj_matrix.pdb"
+  "CMakeFiles/4__DFS_in_adj_matrix.dir/main.c.obj"
+  "lib4__DFS_in_adj_matrix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/4__DFS_in_adj_matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14. Graphs/4. DFS in adj matrix/cmake-build-debug/CMakeFiles/4__DFS_in_adj_matrix.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 4__DFS_in_adj_matrix.
+# This may be replaced when dependencies are built.
